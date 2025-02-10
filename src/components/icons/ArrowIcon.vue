16fc12e2c8faae388de73f0cc9caace470b1164e
Akai <akai@sunsino.com.tw>
2025-02-11 00:44:19 +0800

    feat: create project about vue

---
+++ src/components/icons/ArrowIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { IconProps } from "./types";
+
+const props = withDefaults(defineProps<IconProps>(), {
+  fillColor: "#00b15d",
+});
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    :stroke="props.fillColor"
+    stroke-width="4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <polyline points="19 12 12 19 5 12"></polyline>
+  </svg>
+</template>
